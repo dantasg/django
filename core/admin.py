636b499d5645f105ebd89512648f56648f207870
Genival Dantas <genineto10@gmail.com>
2021-11-22 13:00:00 -0300

    Atualizando segurança

--- core/admin.py
+++ core/admin.py
@@ -8,3 +8,4 @@ from .models import Produto, Cliente
 admin.site.register(Produto)
 admin.site.register(Cliente)
 
+print('Genival!')
--- django2/settings.py
+++ django2/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%x&qt%twfc_g$5qnk9jr4=u)_3#^k3ic^kck)mh@at$68epce2'
+from .env import secrety
+SECRET_KEY = secrety
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
